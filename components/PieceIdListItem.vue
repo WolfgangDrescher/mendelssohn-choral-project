96b9caf58db723f85c4944727fed19eda5445c63
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-10-18 00:18:56 +0200

    Add PieceIdListItem wrapper component

---
+++ components/PieceIdListItem.vue
@@ -0,0 +1,10 @@
+<script setup>
+const props = defineProps({
+    id: String, 
+});
+const { data: piece } = await useAsyncData(`pieces/${props.id}`, () => queryContent(`/pieces/${props.id}`).findOne());
+</script>
+
+<template>
+    <PieceListItem :piece/>
+</template>
