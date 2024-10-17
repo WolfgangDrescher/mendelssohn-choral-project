e8ac1e248ad81c6f2a5434754cfd434cd672843d
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-10-18 00:17:39 +0200

    Add SidebarContent component

---
+++ components/SidebarContent.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex gap-6">
+        <div class="grow-0 shrink-0 basis-1/4">
+            <slot name="aside"></slot>
+        </div>
+        <div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
