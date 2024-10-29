c896d618614cfbfc3b4cf32951c79ba2a481d2fc
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-10-29 23:59:05 +0100

    Improve filters

--- pages/piece/[id].vue
+++ pages/piece/[id].vue
@@ -7,10 +7,50 @@ const [prevPiece, nextPiece] = surroundData.value;
 
 const score = ref();
 
+const filters = reactive({
+    hideLyrics: false,
+    hideDynamics: false,
+    pianoReduction: false,
+    autobeam: false,
+    bassstufen: false,
+    fb: false,
+});
+
 onMounted(async () => {
     const response = await fetch(piece.value.localRawFile);
     const kern = await response.text();
     score.value = kern;
+});
+
+const formattedData = computed(() => {
+    const usedFilters = [];
+
+    if (filters.hideLyrics) {
+        usedFilters.push('extract -I "**text"');
+    }
+
+    if (filters.hideDynamics) {
+        usedFilters.push('extract -I "**dynam"');
+    }
+    
+    if (filters.pianoReduction) {
+        usedFilters.push('satb2gs');
+    }
+
+    if (filters.autobeam) {
+        usedFilters.push('autobeam');
+    }
+
+    if (filters.bassstufen) {
+        usedFilters.push('deg -k 1 --circle');
+    }
+
+    if (filters.fb) {
+        usedFilters.push('fb -canr --above');
+    }
+
+    return score.value ? `${score.value}
+${(usedFilters ?? []).map(filter => `!!!filter: ${filter}`).join('\n')}` : null;
 });
 </script>
 
@@ -42,6 +82,14 @@ onMounted(async () => {
             </div>
 
             <div class="flex items-center gap-4">
+                <div class="flex gap-6">
+                    <UCheckbox v-model="filters.hideLyrics" :label="$t('hideLyrics')" />
+                    <UCheckbox v-model="filters.hideDynamics" :label="$t('hideDynamics')" />
+                    <UCheckbox v-model="filters.pianoReduction" :label="$t('pianoReduction')" />
+                    <UCheckbox v-model="filters.autobeam" :label="$t('autobeam')" />
+                    <UCheckbox v-model="filters.bassstufen" :label="$t('bassstufen')" />
+                    <UCheckbox v-model="filters.fb" :label="$t('figuredBassFigures')" />
+                </div>
                 <div class="flex gap-2 ml-auto">
                     <UButton :to="`https://github.com/WolfgangDrescher/mendelssohn-choral-works/blob/master/kern/${piece.id}.krn`" target="_blank">
                         Auf GitHub öffnen
@@ -53,8 +101,8 @@ onMounted(async () => {
             </div>
 
             <VerovioCanvas
-                v-if="score"
-                :data="score"
+                v-if="formattedData"
+                :data="formattedData"
                 :options="{
                     spacingSystem: 15,
                     pageMarginLeft: 30,
