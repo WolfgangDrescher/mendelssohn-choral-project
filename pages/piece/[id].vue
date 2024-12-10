6470ea8bb77e2419f406a4daad294b2d1f8e39ab
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-12-10 23:28:15 +0100

    Add midi player

--- pages/piece/[id].vue
+++ pages/piece/[id].vue
@@ -112,6 +112,7 @@ ${(usedFilters ?? []).map(filter => `!!!filter: ${filter}`).join('\n')}` : null;
 
             <div class="flex flex-col md:flex-row items-center gap-4">
                 <div class="shrink-0 flex gap-2 ml-auto md:order-2">
+                    <MidiPlayer :url="piece.localRawFile" class="text-2xl"/>
                     <UButton :to="`https://github.com/WolfgangDrescher/mendelssohn-choral-works/blob/master/kern/${piece.id}.krn`" target="_blank">
                         Auf GitHub öffnen
                     </UButton>
