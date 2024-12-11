b2e9caf01ba8485180b72e9ce3d952f78d6c3a7c
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-12-11 16:34:12 +0100

    Show scores for transitions

--- pages/modulations.vue
+++ pages/modulations.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { onKeyStroke } from '@vueuse/core';
+
 const { data } = await useAsyncData('pieces', () => queryContent('/pieces').find());
 const { data: modulationsData } = await useAsyncData(`modulations`, () => queryContent(`/modulations`).findOne(), {deep: false });
 const { data: sequencesData } = await useAsyncData(`sequences-data`, () => queryContent(`/sequences-data`).findOne(), {deep: false });
@@ -84,7 +86,76 @@ function chartClickHandler(chart, event) {
 }
 
 const filteredTransition = computed(() => {
-    return transitionsData.value.transitions.filter(t => t.currentDeg === filter.currentDeg && t.nextDeg === filter.nextDeg)[0];
+    return transitionsData.value.transitions.filter(t => t.currentDeg === filter.currentDeg && t.nextDeg === filter.nextDeg)[0] ?? {};
+});
+
+const openModal = ref(false);
+const modalScoreData = ref();
+async function loadScoreData(item) {
+    modalScoreData.value = null;
+    const data = await $fetch(`/kern/mendelssohn-choral-works/${item.id}.krn`, { parseResponse: (txt) => txt });
+    const lines = data.split('\n');
+
+    const i = item.lineNumber - 1;
+    const tokens = lines[i].split('\t');
+    tokens.forEach((_, tokenIndex) => {
+        const resolvedLineIndex = getResolvedTokenLineIndex(i, tokenIndex, lines);
+        if (resolvedLineIndex) {
+            lines[resolvedLineIndex] = lines[resolvedLineIndex].split('\t').map((token, ti) => {
+                if (ti === tokenIndex &&/^[\[\(]?\d+/.test(token)) {
+                    return `${token}@`;
+                }
+                return token;
+            }).join('\t');
+        }
+    });
+    
+    lines.push('!!!filter: deg -k 1 --circle');
+    lines.push('!!!filter: fb -cnl --above');
+    lines.push('!!!filter: extract -I text');
+    lines.push('!!!filter: extract -I dynam');
+    lines.push('!!!filter: autobeam');
+    lines.push('!!!filter: satb2gs');
+    lines.push('!!!RDF**kern: @ = marked note color="#ef4444');
+    modalScoreData.value = lines.join('\n');
+    openModal.value = `${item.id}-${item.beat}`;
+}
+
+function tokenIsDataRecord(line, includeNullToken = false) {
+    return !line.startsWith('!') && !line.startsWith('*') && !line.startsWith('=') && !(!includeNullToken && line === '.');
+}
+
+function getResolvedTokenLineIndex(lineIndex, spineIndex, lines) {
+    for (let i = lineIndex; i >= 0; i--) {
+        const token = lines[i].split('\t')[spineIndex];
+        if (token && tokenIsDataRecord(token)) {
+            return i;
+        }
+    }
+    return null;
+}
+
+function closeModal() {
+    openModal.value = null
+    activeIndex = null;
+    modalScoreData.value = null;
+}
+
+let activeIndex = null;
+function loadIndex(index) {
+    const item = filteredTransition.value.items[index]; 
+    if (item) {
+        loadScoreData(item);
+        activeIndex = index;
+    }
+}
+
+onKeyStroke('ArrowLeft', () => {
+    if (activeIndex !== null) loadIndex(activeIndex - 1);
+});
+
+onKeyStroke('ArrowRight', () => {
+    if (activeIndex !== null) loadIndex(activeIndex + 1);
 });
 </script>
 
@@ -122,7 +193,19 @@ const filteredTransition = computed(() => {
                 <div class="h-[300px]">
                     <Chart :config="transitionsConfig" @chart-click="chartClickHandler" />
                 </div>
-                {{ filteredTransition }}
+                <div v-if="filteredTransition.items" class="flex flex-wrap gap-2 mt-4">
+                    <UButton v-for="(item, index) in filteredTransition.items" :key="`${item.id}-${item.beat}`" @click="loadIndex(index)">
+                        {{ `${item.id}-${item.beat}` }}
+                        <Modal v-if="openModal === `${item.id}-${item.beat}`" @close="closeModal">
+                            <MidiPlayer :url="`/kern/mendelssohn-choral-works/${item.id}.krn`" class="text-2xl" />
+                            <VerovioCanvas v-if="modalScoreData"  :data="modalScoreData" :scale="35" :page-margin="50" :key="modalScoreData" />
+                            <div class="flex gap-4">
+                                <UButton v-if="filteredTransition.items[index - 1]" @click="loadIndex(index - 1)" class="mr-auto">{{ $t('previous') }}</UButton>
+                                <UButton v-if="filteredTransition.items[index + 1]" @click="loadIndex(index + 1)" class="ml-auto">{{ $t('next') }}</UButton>
+                            </div>
+                        </Modal>
+                    </UButton>
+                </div>
             </template>
 
         </UTabs>
