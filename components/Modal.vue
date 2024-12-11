01cda1065e79a7167469e192848f0bef36e6be9e
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-12-11 16:39:57 +0100

    Remove aria-hidden attribute

--- components/Modal.vue
+++ components/Modal.vue
@@ -20,7 +20,7 @@ onUnmounted(() => {
 <template>
     <ClientOnly>
         <Teleport to="body">
-            <div @click="$emit('close')" tabindex="-1" aria-hidden="true" class="overflow-y overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full h-screen max-h-full bg-black/80">
+            <div @click="$emit('close')" tabindex="-1" class="overflow-y overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full h-screen max-h-full bg-black/80">
                 <div class="relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-2xl max-h-full" @click.stop>
                     <div class="p-4">
                         <div class="bg-white rounded shadow">
