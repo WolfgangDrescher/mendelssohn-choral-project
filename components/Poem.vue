5c0228aae7861fcc58fd91a9f10b304509c99d25
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-12-12 11:19:47 +0100

    Load poems from nuxt content

---
+++ components/Poem.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+    poem: Object,
+});
+</script>
+
+<template>
+    <div>
+        <div v-if="poem.author">{{ poem.author }}</div>
+        <div v-if="poem.book">{{ poem.book }}</div>
+        <Subheading>{{ poem.title }}</Subheading>
+        <div class="font-serif ml-8">
+            <div v-for="item in poem.content">
+                <p v-if="item.type === 'strophe'" class="my-6">
+                    <div v-for="verse in item.verses" v-text="verse"></div>
+                </p>
+                <h3 v-if="item.type === 'heading'" v-text="item.text" class="font-bold text-lg mt-8"></h3>
+            </div>
+        </div>
+        <NuxtLink :to="poem.url" v-text="poem.url" target="_blank" />
+        <div v-if="poem.copyright" class="text-sm mt-8" v-text="poem.copyright"></div>
+    </div>
+</template>
--- pages/poems.vue
+++ pages/poems.vue
@@ -1,75 +1,48 @@
+<script setup>
+const { data: poems } = await useAsyncData('poems', () => queryContent('/poems/').sort({
+    author: 1,
+    book: 1,
+    title: 1,
+}).find());
+
+const poemsGroupedByAuthor = computed(() => {
+    return Object.entries(poems.value.reduce((accumulator, obj) => {
+		const key = obj.book.toLowerCase().includes('des knaben wunderhorn') ? 'Des Knaben Wunderhorn' : obj.author;
+		if (!accumulator[key]) {
+			accumulator[key] = [];
+		}
+		accumulator[key].push(obj);
+		return accumulator;
+	}, {}));
+});
+
+const selectedPoem = ref();
+</script>
+
 <template>
     <UContainer>
         <Heading>{{ $t('poems') }}</Heading>
         <NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh1/wh1.html">https://www.projekt-gutenberg.org/arnim/wh1/wh1.html</NuxtLink>
 
-        <Subheading>Des Knaben Wunderhorn</Subheading>
-        <ul>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh1/wh1061.html">Liebesprobe</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh1/wh1063.html">Der Falke</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh1/wh1077.html">Wassersnoth</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh1/wh1093a.html">Frau Nachtigal</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh1/wh1124.html">Das Bäumlein</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh1/wh1161a.html">Das Todaustreiben</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh1/wh1226b.html">Frühlingsbeklemmung</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh1/wh1077.html">Martinsgans</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh1/wh1235b.html">Marienwürmchen</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh1/wh1257.html">Das römische Glas</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh1/wh1306b.html">Jagdglück</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh1/wh1327.html">Nächtliche Jagd</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh1/wh1329.html">Knabe und Veilchen</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh1/wh1374.html">Die schlechte Liebste</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh2/wh2024.html">Soldatenglück</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh2/wh2029.html">Wär ich ein Knab geboren</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh2/wh2037.html">Vom Wasser und vom Wein</NuxtLink></li>
-            <li class="font-bold"><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh2/wh2061.html">Selbstgefühl</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh2/wh2196.html">Das glaubst du nur nicht</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh2/wh2229a.html">Gedankenstille</NuxtLink></li>
-            <li class="font-bold"><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh2/wh2412.html">Fuge</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh3/wh2229a.html">Einsiedler</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh3/wh3056.html">Bei Nacht sind alle Kühe schwarz</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh3/wh3061.html">Die Schwalben</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh3/wh3071c.html">Wiederhall</NuxtLink></li>
-        </ul>
+        <div class="grid grid-cols-2 gap-4">
+            <div>
+                <div v-for="group in poemsGroupedByAuthor">
+                    <Subheading>{{ group[0] }}</Subheading>
+                    <ul>
+                        <li v-for="poem in group[1]">
+                            <NuxtLink @click="selectedPoem = poem" class="cursor-pointer">
+                                {{ poem.title }}
+                            </NuxtLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <UCard v-if="selectedPoem">
+                    <Poem :poem="selectedPoem" />
+                </UCard>
+            </div>
+        </div>
 
-        <Subheading>Ludwig Hölty</Subheading>
-        <ul>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/arnim/wh3/whkl093d.html">Ach wenn ich doch ein Täublein wär</NuxtLink></li>
-        </ul>
-
-        <Subheading>Clemens Brentano</Subheading>
-        <ul>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/brentano/gedichte/chap004.html">Phantasie</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/brentano/gedichte/chap017.html">[Die Klage, sie wecket]</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/brentano/gedichte/chap033.html">[Die Liebe lehrt]</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/brentano/gedichte/chap037.html">[Lieb und Leid im leichten Leben]</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/brentano/gedichte/chap044.html">[Süßer Mai du Quell des Lebens]</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/brentano/gedichte/chap056.html">[Singet leise, leise, leise]</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/brentano/gedichte/chap064.html">[Die Welt war mir zuwider]</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/brentano/gedichte/chap117.html">[Wenn die Sonne weggegangen]</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/brentano/gedichte/chap125.html">[O kühler Wald]</NuxtLink></li>
-        </ul>
-        
-        <Subheading>Annette von Droste-Hülshoff</Subheading>
-        <ul>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/droste/1844/chap035.html">Am Weiher</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/droste/1844/chap053.html">Not</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/droste/1844/chap087.html">Die Schmiede</NuxtLink></li>
-        </ul>
-
-        <Subheading>Joseph von Eichendorf</Subheading>
-        <ul>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/eichndrf/gedichte/chap002.html">Allgemeines Wandern</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/eichndrf/gedichte/chap021.html">Die Nacht</NuxtLink></li>
-            <li><NuxtLink href="https://www.projekt-gutenberg.org/eichndrf/gedichte/chap027.html">Wandernder Dichter</NuxtLink></li>
-        </ul>
-
-        <Subheading>Adalbert von Chamisso</Subheading>
-        <ul>
-            <li><NuxtLink href="http://www.zeno.org/nid/20004646851">Morgentau</NuxtLink></li>
-            <li><NuxtLink href="http://www.zeno.org/nid/20004647165">Sternschnuppe</NuxtLink></li>
-            <li><NuxtLink href="http://www.zeno.org/nid/20004647378">Die Quelle</NuxtLink></li>
-            <li><NuxtLink href="http://www.zeno.org/nid/20004647971">Ungewitter</NuxtLink></li>
-        </ul>
     </UContainer>
 </template>
